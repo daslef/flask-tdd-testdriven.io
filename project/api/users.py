4e42443124d804b2564de6bb3c3a444bf02d5ae9
Lex Trofimov <daslef93@gmail.com>
2020-02-11 09:58:36 +0300

    users_post validation added, tests passed

--- project/api/users.py
+++ project/api/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from flask_restplus import Resource, Api
+from flask_restplus import Resource, Api, fields
 
 from project import db
 from project.api.models import User
@@ -7,11 +7,25 @@ from project.api.models import User
 users_blueprint = Blueprint('users', __name__)
 api = Api(users_blueprint)
 
+user = api.model('User', {
+    'id': fields.Integer(readOnly=True),
+    'username': fields.String(required=True),
+    'email': fields.String(required=True),
+    'created_date': fields.DateTime,
+})
+
 class UsersList(Resource):
+    @api.expect(user, validate=True)
     def post(self):
         post_data = request.get_json()
         username = post_data.get('username')
         email = post_data.get('email')
+        response_object = {}
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            response_object['message'] = 'Sorry. That email already exists.'
+            return response_object, 400
         db.session.add(User(username=username, email=email))
         db.session.commit()
         response_object = { 'message': f'{email} was added!' }
--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -18,7 +18,7 @@ def test_add_user_invalid_json(test_app, test_database):
 
 def test_add_user_invalid_json_keys(test_app, test_database):
     client = test_app.test_client()
-    resp = client.post('/users', data=json.dumps({}), content_type='application/json')
+    resp = client.post('/users', data=json.dumps({'email':'daslef93@gmail.com'}), content_type='application/json')
     data = json.loads(resp.data.decode())
     assert resp.status_code == 400
     assert 'Input payload validation failed' in data['message']
